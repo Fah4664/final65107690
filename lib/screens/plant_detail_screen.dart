d685719a1b6ddb29d056680c23ab110e07b2df15
Fah4664 <warapornzz4664@gmail.com>
2024-09-28 15:45:05 +0700

    Update plant_detail_screen.dart

--- lib/screens/plant_detail_screen.dart
+++ lib/screens/plant_detail_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import '../helpers/database_helper.dart'; // นำเข้า DatabaseHelper
+import '../helpers/database_helper.dart';
+import 'edit_plant_screen.dart';
 
 class PlantDetailScreen extends StatelessWidget {
   final Map<String, dynamic> plant;
-  final DatabaseHelper _dbHelper = DatabaseHelper(); // สร้างอินสแตนซ์ DatabaseHelper
+  final DatabaseHelper _dbHelper = DatabaseHelper();
 
   PlantDetailScreen({required this.plant});
-
   // ฟังก์ชันลบข้อมูลพืช
+
   void _deletePlant(BuildContext context) async {
     // ลบพืชจากฐานข้อมูล
     await _dbHelper.deletePlant(plant['plantID']); // ใช้ plantID ในการลบ
@@ -21,31 +22,71 @@ class PlantDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(plant['plantName']),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            plant['plantImage'] != null
-                ? Image.network(plant['plantImage'])
-                : Icon(Icons.nature, size: 100),
-            SizedBox(height: 20),
-            Text(
-              plant['plantScientific'] ?? 'Scientific name not available',
-              style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
+    return FutureBuilder<Map<String, dynamic>>(
+      future: _dbHelper.getPlantDetails(plant['plantID']),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (!snapshot.hasData) {
+          return Center(child: Text('ไม่พบข้อมูล.'));
+        }
+
+        var details = snapshot.data!;
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(plant['plantName']),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () => _deletePlant(context),
+              ),
+            ],
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                plant['plantImage'] != null
+                    ? Image.network(plant['plantImage'])
+                    : Icon(Icons.nature, size: 100),
+                SizedBox(height: 20),
+                Text('Plant Name: ${details['plantName']}', style: TextStyle(fontSize: 20)),
+                Text('Scientific Name: ${details['plantScientific']}', style: TextStyle(fontSize: 20)),
+                Text('Component Name: ${details['componentName'] ?? 'N/A'}', style: TextStyle(fontSize: 20)),
+                Text('Land Use Type Name: ${details['landUseTypeName']}', style: TextStyle(fontSize: 20)),
+                Text('Land Use Type Description: ${details['landUseTypeDescription']}', style: TextStyle(fontSize: 20)),
+                Text('Land Use Description: ${details['landUseDescription']}', style: TextStyle(fontSize: 20)),
+                SizedBox(height: 20),
+              ],
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _deletePlant(context), // เรียกใช้ฟังก์ชันลบเมื่อคลิก
-              child: Text('Delete Plant'),
-              style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 230, 114, 106)), // แก้ไขที่นี่
+          ),
+          bottomNavigationBar: BottomAppBar(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ViewEditPlantScreen(plantData: plant),
+                      ),
+                    );
+                  },
+                  child: Text('แก้ไข'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _deletePlant(context), // เรียกใช้ฟังก์ชันลบ
+                  child: Text('ลบ'),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
